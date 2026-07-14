f262c34c6203e481d3c850ac6ee9cafd92c45ab1
Sinazo-M98 <snazomgee@gmail.com>
2026-07-14 10:52:23 +0000

    Update

--- 3. Data Processing/Bright TV-Viewership Notebook.sql
+++ 3. Data Processing/Bright TV-Viewership Notebook.sql
@@ -1,6 +1,17 @@
 -- Databricks notebook source
+-----------------------------------------------------
+Checking all the columns in the table viewership
+---------------------------------------------------
 SELECT *
 FROM workspace.database.viewership
+-------------------------------------------------
+Checking all the columns in the table viewership
+-----------------------------------------------
+SELECT *
+FROM workspace.database.viewership
+WHERE UserID0 IS NULL
+
+
 
 SELECT RecordDate2,
 TO_DATE(RecordDate2) AS watch_date,
