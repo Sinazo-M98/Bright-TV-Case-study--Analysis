ca901bf4aea76d90290a4817aa1742f4bdaeccc1
Sinazo-M98 <snazomgee@gmail.com>
2026-07-15 12:29:35 +0000

    update

--- 3. Data Processing/Bright TV-Viewership Notebook.sql
+++ 3. Data Processing/Bright TV-Viewership Notebook.sql
@@ -4,12 +4,45 @@ Checking all the columns in the table viewership
 ---------------------------------------------------
 SELECT *
 FROM workspace.database.viewership
+LIMIT 10;
 -------------------------------------------------
-Checking all the columns in the table viewership
+Checking for NULL values in my dataset -table Viewership
 -----------------------------------------------
 SELECT *
 FROM workspace.database.viewership
-WHERE UserID0 IS NULL
+WHERE UserID0 IS NULL 
+OR userid4 IS NULL
+---------------------------------------------------
+Verify why we have two columns of two USERid
+--------------------------------------------------
+SELECT *
+FROM workspace.database.viewership
+WHERE UserID0 <> userid4
+---------------------------------------------
+Check for duplicates
+-----------------------------------------------------
+SELECT COUNT (*),UserID0,RecordDate2
+FROM workspace.database.viewership
+GROUP BY UserID0,RecordDate2
+COUNT(*) AS duplicate_count
+HAVING COUNT(*)>1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
